6cc598528eebecda5a1e7082b2668c01bacd7377
Sam <sam.a.menza@gmail.com>
2015-04-15 18:25:08 -0400

    Took out all individual category templates, started general category.php

--- category.php
+++ category.php
@@ -2,62 +2,53 @@
 /**
 * Huskytech Category Template
 */
+?> 
+<p> <?php $title=single_cat_title("",false);?> </p> 
+<?php $term = get_term_by('name', $title, 'category');?>
 
-get_header(); ?> 
-<?php 
-if ( have_posts() ) : ?>
-
-<div class="category-temp topcategoryrow1 row-fluid">
-	<?php $this_category = get_category($cat); ?>
-	<?php if ($this_category->category_parent == 0) { ?>
-
-	<?php } else { ?>
-	<?php $parent_category = get_category($this_category->category_parent); ?>
-        <?php $parent_name = $parent_category->cat_name; 
-			  $child_name=$this_category->cat_name;?>
-	<?php } ?>
-    <?php if(substr( $child_name, 0, 3 ) == "All"):
-			$child_name='All';
-		else:
-			
-		endif;?>
-	<h2 class="subcategory-title category-temp category-title"> <?php echo $parent_name.':'; echo $child_name; ?></h2>
-</div>
-
-    <div class="category-temp topcategoryrow2 row-fluid">
-          <div class="span3">
-           <form class="category-temp" action="<?php bloginfo('url'); ?>/" method="get">
-                  <?php 
-                  $idObj = $parent_category; 
-                  $id = $idObj->term_id;
-                  ?>
-                      <?php
-                        $catargs = array(
-                            'show_count' => 0,
-                            'orderby' => 'name',
-                            'echo' => 0,
-                            'child_of' => $id,
-                        );
-                        $select = wp_dropdown_categories($catargs);
-                        $select = preg_replace("#<select([^>]*)>#", "<select$1 onchange='return this.form.submit()'>", $select);
-                        echo $select;
-                      ?>
-                    <noscript><input type="submit" value="View" /></noscript>
-             </form>
-          </div>
+  <div class="wrapper">
+      <?php 
+      get_header();
+	  $current_url=get_stylesheet_directory_uri();
+	  $main_url=$current_url.'/img/services.gif'; ?>
+    <div class="category-temp topcategoryrow1 row-fluid">
+    	<img class="category-temp category-image" src="<?php echo $main_url ?>" alt="software/accounts icon">
+    	<h2 class="category-temp category-title"> <?php echo $title; ?> </h2>
     </div>
-<div class="rowbottom topcategoryrow3 category-temp row-fluid">
-  <?php
-    while ( have_posts() ) : the_post();
-      get_template_part('content','search');
-  ?>
-    <?php endwhile;
-    else: ?>
-    <p>No articles in this area</p>
-    <?php endif; ?>
-</div>
+    <div class="rowbottom category-temp topcategoryrow2 row-fluid">
+            <div class="span2">
+            <form class="category-temp" action="<?php bloginfo('url'); ?>/" method="get">
+            <?php 
+			  $term = get_term_by('name', $title, 'category');
+			  $id = $term->term_id;
+			?>
+				<?php
+				  $catargs = array(
+					  'show_count' => 0,
+					  'orderby' => 'name',
+					  'echo' => 0,
+					  'child_of' => $id,
+				  );
+				  $select = wp_dropdown_categories($catargs);
+				  $select = preg_replace("#<select([^>]*)>#", "<select$1 onchange='return this.form.submit()'>", $select);
+				  echo $select;
+				?>
+              <noscript><input type="submit" value="View" /></noscript>
+              </form>
+        </div>
+    </div>
+    <div class="rowbottom topcategoryrow3 category-temp row-fluid">
+		  <?php $software_query = new WP_Query( array( 'posts_per_page' => '10', 'cat' => $id, 'orderby' => 'date', 'include_children'=>true) ); ?>
+			<?php if ( $software_query->have_posts() ) : ?>
+  				<?php while ( $software_query->have_posts() ) : $software_query->the_post(); ?>
+               				<?php get_template_part('content','search'); ?>
+              	<?php endwhile; ?>
+     		<?php endif; ?>
+     		<?php wp_reset_postdata();
+			wp_reset_query(); ?>
+    </div>
 
-<section class="rowbottom" id="catagories">
 <?php get_template_part('content','categories'); ?>
-</section>
+        
 <?php get_footer(); ?>
+</div>
--- edu.uconn.dev.huskytech/themes/huskytechkb/category.php
+++ edu.uconn.dev.huskytech/themes/huskytechkb/category.php
@@ -1,23 +1,26 @@
-  <?php
-  /*
-  Template Name: Windows OS
-  */
-  ?>
+<?php
+/**
+* Huskytech Category Template
+*/
+?> 
+<p> <?php $title=single_cat_title("",false);?> </p> 
+<?php $term = get_term_by('name', $title, 'category');?>
+
   <div class="wrapper">
       <?php 
       get_header();
 	  $current_url=get_stylesheet_directory_uri();
-	  $main_url=$current_url.'/img/windows.jpg'; ?>
+	  $main_url=$current_url.'/img/services.gif'; ?>
     <div class="category-temp topcategoryrow1 row-fluid">
-    	<img class="category-temp category-image" src="<?php echo $main_url ?>" alt="apple icon">
-    	<h2 class="category-temp category-title"> Windows OS</h2>
+    	<img class="category-temp category-image" src="<?php echo $main_url ?>" alt="software/accounts icon">
+    	<h2 class="category-temp category-title"> <?php echo $title; ?> </h2>
     </div>
     <div class="rowbottom category-temp topcategoryrow2 row-fluid">
             <div class="span2">
             <form class="category-temp" action="<?php bloginfo('url'); ?>/" method="get">
             <?php 
-			  $idObj = get_category_by_slug('windowsos'); 
-			  $id = $idObj->term_id;
+			  $term = get_term_by('name', $title, 'category');
+			  $id = $term->term_id;
 			?>
 				<?php
 				  $catargs = array(
@@ -35,7 +38,7 @@
         </div>
     </div>
     <div class="rowbottom topcategoryrow3 category-temp row-fluid">
-		  <?php $software_query = new WP_Query( array( 'posts_per_page' => '10', 'category_name' => 'windowsos', 'orderby' => 'date')); ?>
+		  <?php $software_query = new WP_Query( array( 'posts_per_page' => '10', 'cat' => $id, 'orderby' => 'date', 'include_children'=>true) ); ?>
 			<?php if ( $software_query->have_posts() ) : ?>
   				<?php while ( $software_query->have_posts() ) : $software_query->the_post(); ?>
                				<?php get_template_part('content','search'); ?>
